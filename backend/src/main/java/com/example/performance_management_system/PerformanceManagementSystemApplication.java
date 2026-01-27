cb15d365958960b9cfa7b7fa3511a3f6f7f682d1
Kapil Yadav <kapil.yadav@bounteous.com>
2026-01-27 10:57:49 +0530

    Describe what you changed

---
+++ backend/src/main/java/com/example/performance_management_system/PerformanceManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.performance_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerformanceManagementSystemApplication.class, args);
+	}
+
+}
